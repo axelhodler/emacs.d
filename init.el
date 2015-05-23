c241a813ff0a437c32dd79cf393b87286d8383ce
xorrr <derxor@gmail.com>
2015-05-23 23:20:50 +0200

    load js2-refactor-mode when js2-mode is loaded

--- init.el
+++ init.el
@@ -35,6 +35,7 @@
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 
 (require 'js2-refactor)
+(add-hook 'js2-mode-hook #'js2-refactor-mode)
 ;; eg. extract function with `C-c C-m ef`.
 (js2r-add-keybindings-with-prefix "C-c C-m")
 
